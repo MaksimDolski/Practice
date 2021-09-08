6fec97c21cb5cc7bf28e15ae58661312c9241cdd
Maksim <maxdolskI@gmail.com>
2021-09-08 13:05:21 +0300

    urlDuplicated

---
+++ urlDuplicated/urlDuplicated.js
@@ -0,0 +1,9 @@
+function dublicateParam(url) {
+    const urlSplit = url.split('&')
+    const lastRemove = urlSplit.pop()
+    let urlReady = urlSplit.toString()
+    
+    console.log(urlReady)
+    }
+
+    dublicateParam("https://devtailor.com?first=1&second=2&first=2")
