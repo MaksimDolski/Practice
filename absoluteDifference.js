2f500273c5c97f8e21984aee45a66022d7009b6a
Maksim <maxdolskI@gmail.com>
2021-09-08 14:50:22 +0300

    new

---
+++ absoluteDifference.js
@@ -0,0 +1,11 @@
+function getAbsoluteDifference(num) {
+let result
+if(num > 19) {
+ result = (num - 19) * 3
+} else {
+    result = num - 19
+}
+return result
+}
+
+console.log(getAbsoluteDifference(5));
