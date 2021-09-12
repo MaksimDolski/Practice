f6361353d1cc96b3efd882542b567ffc9baa9701
Maksim <maxdolskI@gmail.com>
2021-09-12 11:55:58 +0300

    If the sum is in the range 50..80 return 65 other wise return 80

---
+++ sumOfTheInt.js
@@ -0,0 +1,10 @@
+function sumOfTheInt(num1, num2) {
+    let result = num1 + num2
+    if (result >= 50 && result <= 80) {
+        return 65
+    } else {
+        return 80
+    }
+}
+
+console.log(sumOfTheInt(50,31));
