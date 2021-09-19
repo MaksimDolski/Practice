894a22afdf8c0c079e3cdf477d6a615502bffcaa
Max <38081990+MaksimDolski@users.noreply.github.com>
2021-09-19 21:45:31 +0300

    Update Capitalize.js

--- Capitalize.js
+++ Capitalize.js
@@ -1,13 +1,13 @@
 function firstLetterCapitalize(str) {
 
 
-let strSplit = str.split(" ")
-for(let i = 0; i < strSplit.length; i++) {
-strSplit[i] = strSplit[i].charAt(0).toUpperCase() + strSplit[i].slice(1)
+let strToUpperCase = str.split(" ")
+for(let i = 0; i < strToUpperCase.length; i++) {
+strToUpperCase[i] = strToUpperCase[i].charAt(0).toUpperCase() + strToUpperCase[i].slice(1)
+strJoin = strToUpperCase.join(" ")
 
 }
-return strSplit
-
+return strJoin
 }
 
-console.log(firstLetterCapitalize("This function slices a given string from a specified “start” position until the specified “end” position."))
+console.log(firstLetterCapitalize("This function slices a given string from a specified position until the specified position."))
